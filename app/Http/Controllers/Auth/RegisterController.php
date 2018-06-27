faac077c0a7fdcc6071c69c3b1c420528a805916
Harrison Favour <favour.harrison@tenece.com>
2018-06-27 12:17:05 +0100

    changed login redirect to /home

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/home';
 
     private $claimsService;
     private $referralService;
