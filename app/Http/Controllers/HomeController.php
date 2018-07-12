8c3e0677af296dab5ce367b3a582ecc61093d53e
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-07-12 17:32:32 +0100

    something

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,11 +44,11 @@ class HomeController extends Controller
         foreach($transactions as $transaction) {
             $transaction->payload = json_decode($transaction->payload);
             $typeId = $transaction->payload->transaction_type_id ?: $transaction->payload->details->transaction_type_id;
-            //$planId = $transaction->payload->transaction_plan_id ?: $transaction->payload->details->transaction_plan_id;
+            $planId = $transaction->payload->transaction_plan_id ?: $transaction->payload->details->transaction_plan_id;
             $type = $typeService->getById($typeId);
-            //$plan = $planService->getById($planId);
+            $plan = $planService->getById($planId);
             $transaction->transaction_type = $type;
-            //$transaction->transaction_plan = $plan;
+            $transaction->transaction_plan = $plan;
             if(preg_match('/deposit/i', $type->name)) {
                 $noOfDeposits += 1;
             } else {
