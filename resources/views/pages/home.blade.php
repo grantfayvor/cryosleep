834c28e730a2233027e09cf761ae1bf245222f6a
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-07-13 07:56:04 +0100

    soemthing

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -478,8 +478,8 @@
                         <tr>
                             <td>Time Stamp</td>
                             <td>Amount</td>
-                            {{--<td>Address</td>--}}
-                            {{--<td>Transaction Plan</td>--}}
+                            <td>Address</td>
+                            <td>Transaction Plan</td>
                             <td>Transaction Type</td>
                             <td>User</td>
 
@@ -490,8 +490,8 @@
                             <tr>
                                 <td>{{$transaction->created_at}}</td>
                                 <td>{{$transaction->amount ?: $transaction->amount_to_pay}}</td>
-                                {{--<td>{{$transaction->payment_address}}</td>--}}
-                                {{--<td>{{$transaction->transaction_plan->name}}</td>--}}
+                                <td>{{$transaction->payment_address}}</td>
+                                <td>{{$transaction->transaction_plan->name}}</td>
                                 <td>{{$transaction->transaction_type->name}}</td>
                                 <td>{{$transaction->user->username}}</td>
                             </tr>
